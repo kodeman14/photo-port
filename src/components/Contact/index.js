efba79aaaae0ae8adf095480933cc1cf5adbf8c9
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2023-02-19 01:05:09 -0500

    control state and submit form (1.4.4)

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react'
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+  const { name, email, message } = formState
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(formState)
+  }
+
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
@@ -16,11 +32,21 @@ function ContactForm() {
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} />
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} />
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
