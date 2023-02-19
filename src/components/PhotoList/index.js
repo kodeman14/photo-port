17171fc820144a01debf7dbb39252644e6c13480
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2023-02-19 01:44:23 -0500

    manage modal state (1.5.4)

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -118,15 +118,20 @@ function PhotoList({ category }) {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
   ])
+
+  const [currentPhoto, setCurrentPhoto] = useState()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   const currentPhotos = photos.filter((photo) => photo.category === category)
 
   const toggleModal = (image, i) => {
-    // current photo
+    setCurrentPhoto({ ...image, index: i })
+    setIsModalOpen(true)
   }
 
   return (
     <div>
-      <Modal />
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       <div className='flex-row'>
         {currentPhotos.map((image, i) => (
           <img
