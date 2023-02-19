64c1f4bc6b41fbb3e9bbdf80f209ebd1b3288fc7
Ankit Curchorcar <3253095+kodeman14@users.noreply.github.com>
2023-02-19 02:00:07 -0500

    Feature/modal (#9)
    * create modal component (1.5.3)
    
    * manage modal state (1.5.4)
    
    * can close modal (1.5.5)
    
    * added tests for modal (1.5.6)

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Modal from '../Modal'
 
 function PhotoList({ category }) {
   const [photos] = useState([
@@ -117,10 +118,22 @@ function PhotoList({ category }) {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
   ])
+
+  const [currentPhoto, setCurrentPhoto] = useState()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   const currentPhotos = photos.filter((photo) => photo.category === category)
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i })
+    setIsModalOpen(!isModalOpen)
+  }
 
   return (
     <div>
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
       <div className='flex-row'>
         {currentPhotos.map((image, i) => (
           <img
@@ -128,6 +141,7 @@ function PhotoList({ category }) {
             alt={image.name}
             className='img-thumbnail mx-1'
             key={image.name}
+            onClick={() => toggleModal(image, i)}
           />
         ))}
       </div>
